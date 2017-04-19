6fed3c3e8961a00c8a2a6eef3806560e41b53084
zakharov <zakharov@debian>
2017-04-19 09:09:38 +0300

    Add print result output

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Python program that finds most frequent word in text file, print word and its count.
+
+# How to Use
+
+Example of script launch on Linux, Python 3.5:
+```#!bash
+$ python lang_frequency.py <path to file>
+```
+Console ouput example:
+```#!bash
+the = 16
+to = 11
+is = 10
+in = 9
+you = 8
+what = 6
+it = 6
+a = 6
+are = 5
+not = 5
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,35 @@
+import os
+import sys
+import string
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as file:
+        text_from_file = file.read()
+    return text_from_file
 
+def remove_punctuation(text):
+    """ This code remove punctuation mark from text"""
+    punct = set(string.punctuation)
+    sans_punct = ''.join(symbl for symbl in text if symbl not in punct)
+    return sans_punct # return list all words from text
 
 def get_most_frequent_words(text):
-    pass
+    return Counter(text.lower().split()).most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+
+    if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
+        filepath = sys.argv[1]
+        text_from_file = load_data(filepath)
+        clean_text = remove_punctuation(text_from_file)
+        most_frequent_words = get_most_frequent_words(clean_text)
+        
+        for word in most_frequent_words:
+            print(word[0],'=', word[1])
+    else:
+        print("File not found.")
+        print("Example launch: python lang_frequency.py <filename>")
+
